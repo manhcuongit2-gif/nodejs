f7013a473310f6c02e6a3bb34700a39ac5a04754
Manh C <manhc@example.com>
2026-01-29 15:59:47 +0700

    video 31

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const app = express();
 const port = process.env.PORT || 8386;
 app.get("/", (req, res) => {
-    res.send("Hello world update 2026");
+    res.send(`<h1 style="color: blue;">Hello World</h1>`);
 });
 app.get("/hoi", (req, res) => {
     res.send("hello tim viec lam");
@@ -12,4 +12,4 @@ app.get("/hoi", (req, res) => {
 app.listen(port, () => {
     console.log(`My app is running on port: ${port}`)
     console.log("env port: ", process.env.PORT);
-});
+}); 
